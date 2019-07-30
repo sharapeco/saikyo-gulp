de233808402f530fa25233c0295330b304529c88
sempre harapeco <scin.ichi@gmail.com>
2019-07-30 18:41:05 +0900

    Add svgmin

--- tasks/image.js
+++ tasks/image.js
@@ -7,10 +7,11 @@ const {head} = require("./utils");
 const plumber = require('gulp-plumber');
 const notifier = require('node-notifier');
 
-// imagemin + pngquant
+// imagemin + pngquant, svgmin
 const imagemin = require("gulp-imagemin");
 const pngquant = require("imagemin-pngquant");
 const webp = require("gulp-webp");
+const svgmin = require("gulp-svgmin");
 
 function makeImageTask(options) {
 	const opt = Object.assign({}, {
@@ -22,7 +23,8 @@ function makeImageTask(options) {
 	}, options);
 
 	const minify_image = function() {
-		return src(`${opt.src}/**/*.{${opt.ext}}`, {base: opt.src})
+		const extWithoutSvg = opt.ext.replace(/^svg|,svg\b/ig, "");
+		return src(`${opt.src}/**/*.{${extWithoutSvg}}`, {base: opt.src})
 		.pipe(changed(opt.dest))
 		.pipe(imagemin([
 			pngquant({
@@ -40,7 +42,22 @@ function makeImageTask(options) {
 		.pipe(dest(opt.dest));
 	};
 
-	const build_image = /\bjpg\b/.test(opt.ext) ? parallel(minify_image, make_webp) : minify_image;
+	const make_svg = function() {
+		return src(`${opt.src}/**/*.svg`, {base: opt.src})
+		.pipe(changed(opt.dest))
+		.pipe(svgmin())
+		.pipe(dest(opt.dest));
+	};
+
+	const tasks = [minify_image];
+	if (/\bjpg\b/i.test(opt.ext)) {
+		tasks.push(make_webp);
+	}
+	if (/\bsvg\b/i.test(opt.ext)) {
+		tasks.push(make_svg);
+	}
+
+	const build_image = parallel.apply(null, tasks);
 
 	const watch_image = function() {
 		return watch([`${opt.src}/**/*.{${opt.ext}}`], build_image);
